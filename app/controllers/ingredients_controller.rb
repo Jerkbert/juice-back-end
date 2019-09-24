2a4ec6ee82b03300fb3ba8d7d2f0860af8772249
github username <github email address>
2019-09-24 11:14:17 -0600

    ingredients contoller updated

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,21 @@
 class IngredientsController < ApplicationController
+    def index
+        ingredients = Ingredient.all 
+        render json: ingredients
+    end
+
+    def show
+        ingredient = Ingredient.find(params[:id])
+        render json: ingredient
+    end
+
+    def create
+        name = params[:name]
+        category = params[:category]
+        nutrition_value = params[:nutrition_value]
+        juice = params[:juice_id]
+        ingredient = Ingredient.create(name: name, category: category, nutrition_value: nutrition_value, juice_id: juice)
+
+        render json: ingredient
+    end
 end
